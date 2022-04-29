b3fcfadd36f013c9f5935d801e0ca4ed3d0a226f
EmmanuelTovurawa <87187766+EmmanuelTovurawa@users.noreply.github.com>
2022-04-29 21:06:18 +0200

    Spring Core

--- taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -3,8 +3,12 @@ package tacos.web;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,13 +53,15 @@ public class DesignTacoController {
 	public String showDesignForm() {
 		return "design";
 	}
-	
+
 	@PostMapping
-	public String processTaco(Taco taco,
-	@ModelAttribute TacoOrder tacoOrder) {
-	tacoOrder.addTaco(taco);
-	log.info("Processing taco: {}", taco);
-	return "redirect:/orders/current";
+	public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder) {
+		if (errors.hasErrors()) {
+			return "design";
+		}
+		tacoOrder.addTaco(taco);
+		log.info("Processing taco: {}", taco);
+		return "redirect:/orders/current";
 	}
 
 	private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
