ee0bec5b4fc035e21030f6eebd981c54b4b795b1
EmmanuelTovurawa <87187766+EmmanuelTovurawa@users.noreply.github.com>
2022-05-03 21:28:15 +0200

    Spring Core

--- taco-cloud/src/main/java/tacos/Taco.java
+++ taco-cloud/src/main/java/tacos/Taco.java
@@ -1,5 +1,6 @@
 package tacos;
-
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.validation.constraints.NotNull;
@@ -9,10 +10,20 @@ import lombok.Data;
 
 @Data
 public class Taco {
-	@NotNull
-	@Size(min=5, message="Name must be at least 5 characters long")
-	private String name;
-	@NotNull
-	@Size(min=1, message="You must choose at least 1 ingredient")
-	private List<Ingredient> ingredients;
+
+  private Long id;
+
+  private Date createdAt = new Date();
+
+  @NotNull
+  @Size(min=5, message="Name must be at least 5 characters long")
+  private String name;
+
+  @Size(min=1, message="You must choose at least 1 ingredient")
+  private List<IngredientRef> ingredients = new ArrayList<>();
+
+  public void addIngredient(Ingredient taco) {
+    this.ingredients.add(new IngredientRef(taco.getId()));
+  }
+
 }
