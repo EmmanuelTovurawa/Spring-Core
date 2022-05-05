c29cae06caf38b384e6dd17bfa1b2a5b1cc87f0d
EmmanuelTovurawa <87187766+EmmanuelTovurawa@users.noreply.github.com>
2022-05-05 22:05:08 +0200

    Spring Core

--- taco-cloud/src/main/java/tacos/TacoOrder.java
+++ taco-cloud/src/main/java/tacos/TacoOrder.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -13,14 +19,16 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 import lombok.Data;
 
 @Data
+@Entity
 public class TacoOrder implements Serializable {
 
   private static final long serialVersionUID = 1L;
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
-  private Date placedAt;
-
+  private Date placedAt = new Date();
 
   @NotBlank(message="Delivery name is required")
   private String deliveryName;
@@ -47,6 +55,7 @@ public class TacoOrder implements Serializable {
   @Digits(integer=3, fraction=0, message="Invalid CVV")
   private String ccCVV;
 
+  @OneToMany(cascade = CascadeType.ALL)
   private List<Taco> tacos = new ArrayList<>();
 
   public void addTaco(Taco taco) {
